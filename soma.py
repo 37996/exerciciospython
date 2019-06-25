42733651732900b599d319f0cc4b5f3d33094442
Manoel <manoel@manoel-HP-EliteBook-8440p>
2019-06-25 13:26:19 +0100

    2 commit

--- soma.py
+++ soma.py
@@ -1,4 +1,6 @@
 a = int(input("Digite um valor:"))
 b = int(input("Digite outro valor:"))
 result = a + b
-print("A soma entre {} E {} é igual a {}".format(a, b, result))
+print("A soma entre {} e {} é igual a {}".format(a, b, result))
+print ("O próximo numero é {} e o anterior é {}".format(result + 1, result - 1))
+print("O dobro de {} é {} e o triplo é {} e a raiz quadrada é {}".format(result, result * 2, result * 3, result ** (1/2)))
