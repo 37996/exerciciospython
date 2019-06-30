32e11ff1546bd3435152cf69f53481831ab311a8
Manoel <manoel@manoel-HP-EliteBook-8440p>
2019-06-30 20:52:41 +0100

    math

--- matematica.py
+++ matematica.py
@@ -1,4 +1,14 @@
 import math
+import random
+import emoji
 num = int(input('Digiteum numero: '))
 raiz = math.sqrt(num)
 print(raiz)
+
+n = random.randint(1, 100)
+print('Numero random: ', n)
+
+print(emoji.emojize("Olá, Mundo! :sunglasses:", use_aliases=True))
+
+f = float(input("Digite um numero float para ver seu inteiro:"))
+print("{} agora é {} ".format(f, math.trunc(f)))
