139a09b0bdb8c4d278d8f16a7d13cc35307d57ab
Manoel Figueiredo <figueiredoufam@gmail.com>
2019-07-02 15:03:19 +0100

    jogoAdivinha

---
+++ jogoAdivinharNumero.py
@@ -0,0 +1,14 @@
+from random import randint
+n = int(randint(1, 20))
+p = 0
+t = 0
+while p != n:
+    p = int(input("Insira o seu palpite: "))
+    t += 1
+    if p == n:
+        print("Acertou! Em {} tentativas ".format(t))
+    elif p < n:
+        print("Chute um valor maior")
+    else:
+        print("Chute um valor menor")
+print("Fim do jogo!")
