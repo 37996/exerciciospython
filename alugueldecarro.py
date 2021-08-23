248fd2ec16896ff272c3c3e4f3109c7c6fad24fd
Manoel Figueiredo <44445806+37996@users.noreply.github.com>
2021-08-23 12:25:14 +0100

    Update alugueldecarro.py

--- alugueldecarro.py
+++ alugueldecarro.py
@@ -1,5 +1,7 @@
 d = int(input('Quantos dias o carro ficou alugado? '))
 c = 60 * d
-km = int(input('Quantos km o carro rodoou? '))
+km = int(input('Quantos km o carro rodou? '))
 ct = (km * 0.15) + c
 print(" O carro ficou alugado por : {} dias e custou {} + 0,15 por Km que totalizou {}".format(d, c, ct))
+
+print("===")
